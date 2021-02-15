18ba6b7184c9dcfb98ea58e7e3c0bed8c2c0395c
Dev_cottonwood <blog_geon@naver.com>
2021-02-15 13:38:19 +0900

    no message

--- 3_5/hash.js
+++ 3_5/hash.js
@@ -4,14 +4,3 @@ console.log('base64:' , crypto.createHash('sha512').update('비밀번호').diges
 console.log('hex:', crypto.createHash('sha512').update('비밀번호').digest('hex'));
 console.log('base64:', crypto.createHash('sha512').update('다른 비밀번호').digest('base64'));
 
-console.log('base64:' , crypto.createHash('sha512').update('비밀번호').digest('base64'));
-console.log('hex:', crypto.createHash('sha512').update('비밀번호').digest('hex'));
-console.log('base64:', crypto.createHash('sha512').update('다른 비밀번호').digest('base64'));
-
-console.log('base64:' , crypto.createHash('sha512').update('비밀번호').digest('base64'));
-console.log('hex:', crypto.createHash('sha512').update('비밀번호').digest('hex'));
-console.log('base64:', crypto.createHash('sha512').update('다른 비밀번호').digest('base64'));
-
-console.log('base64:' , crypto.createHash('sha512').update('비밀번호').digest('base64'));
-console.log('hex:', crypto.createHash('sha512').update('비밀번호').digest('hex'));
-console.log('base64:', crypto.createHash('sha512').update('다른 비밀번호').digest('base64'));
