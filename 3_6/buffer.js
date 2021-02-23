19175e36b5977a572217ba065742e404a5bc8101
Dev_cottonwood <blog_geon@naver.com>
2021-02-23 21:41:11 +0900

    기타 fs 메서드

--- 3_6/buffer.js
+++ 3_6/buffer.js
@@ -3,7 +3,7 @@ console.log('from():', buffer);
 console.log('length:',buffer.length);
 console.log('toString():', buffer.toString());
 
-const array = [Buffer.from('띄엄'),Buffer.from('띄엄'),Buffer.from('띄어쓰기')];
+const array = [Buffer.from('띄엄 '),Buffer.from('띄엄 '),Buffer.from('띄어쓰기')];
 const buffer2 = Buffer.concat(array);
 console.log('concat():', buffer2.toString());
 
