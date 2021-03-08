829f875a984018ee17d396a2d3babc74cfb26e9a
Dev_cottonwood <blog_geon@naver.com>
2021-03-08 23:15:15 +0900

    no message

--- 4_1/restServer.js
+++ 4_1/restServer.js
@@ -3,6 +3,15 @@ const fs = require('fs').promises;
 
 const users = {}; // 데이터 저장용
 
+/* 
+req.method로 HTTP 요청 메서드를 구분하고 있다.
+메서드가 GET이면 다시 req.url로 요청 주소를 구분한다.
+주소가 /일 때는 restFront.html을 제공하고, 주소가 /about이면 about.html파일을 제공한다.
+이외의 경우에는 주소에 적힌 파일을 제공한다. 
+/restFront.js 라면 restFront.js파일을 제공할 것이고, /restFront.css 라면 restFront.css 파일을 제공할 것이다.
+만약 존재하지 않는 파일을 요청했거나 GET 메서드 요청이 아닌 경우라면 404 NOT FOUND 에러가 응답으로 전송된다.
+응답 과정 중에 예기치 못한 에러가 발생한 경우에는 500 에러가 응답으로 전송된다.(실무에서 500을 전송하는 경우는 드물다.)
+*/
 http.createServer(async (req, res) => {
   try {
     if (req.method === 'GET') {
@@ -75,3 +84,5 @@ http.createServer(async (req, res) => {
   .listen(8082, () => {
     console.log('8082번 포트에서 서버 대기 중입니다');
   });
+
+  //
