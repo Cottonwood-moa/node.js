7638ed28416c53ab28b4d26746b5e05884662734
Dev_cottonwood <blog_geon@naver.com>
2021-03-17 10:39:08 +0900

    no message

--- 6_2/app.js
+++ 6_2/app.js
@@ -68,3 +68,21 @@ app.use((err, req, res, next) => {
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
 });
+
+/* 
+설치했던 패키지들을 불러온 뒤 app.use 에 연결한다.
+req res next 같은 것들이 보이지 않아 당황스러울 수 있는데
+미들웨어 내부에 들어있다.
+next도 내부적으로 호출하가에 다음 미들웨어로 넘어갈 수 있다.
+
+dotenv 패키지는 .env 파일을 읽어서 process.env호 만든다.
+dotenv 패키지의 이름이 dot(점) + env 인 이유이다.
+process.env.COOKIE_SECRET에 cookiesecret 값이 할당된다.
+키=값 형식으로 추가하면 된다. process.env 를 별도의 파일로 관리하는 이유는 보안과 설정의 편의성 때문이다.
+비밀 키들을 소스 코드에 그대로 적어두면 소스 코드가 유출되었을 때 키도 같이 유출된다.
+따라서 env 같은 별도의 파일에 비밀 키를 적어두고 dotenv패키지로 비밀 키를 로딩하는 방식으로 관리하곤 한다.
+소스 코드가 유출되더라도, .env 파일만 잘 관리하면 비밀 키는 지킬 수 있다.
+
+각각의 미들웨어를 살펴보자.
+
+*/
