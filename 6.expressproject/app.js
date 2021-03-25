7bbb013be16df4af537a23b5e5cb82b4d4822ab9
cottonwood-moa <blog_geon@naver.com>
2021-03-25 23:47:26 +0900

    no message

--- 6.expressproject/app.js
+++ 6.expressproject/app.js
@@ -52,3 +52,25 @@ app.use((err, req, res, next)=>{
 app.listen(app.get('port'), ()=>{
 console.log(app.get('port'), '번 포트에서 대기중');
 });
+
+app.use((req, res, next)=>{
+    console.log('모든 요청에 다 실행됩니다.');
+    next();
+});
+
+app.get('/',(req,res,next)=>{
+    // res.sendFile(path.join(__dirname, '/index.html'));
+    console.log('GET / 요청에서만 실행됩니다.');
+    next();
+},(req,res)=>{
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+});
+
+app.use((err, req, res, next)=>{
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), ()=>{
+console.log(app.get('port'), '번 포트에서 대기중');
+});
