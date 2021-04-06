a7804cd15219b12e021c688d953c29aa9214f46e
Cottonwood <blog_geon@naver.com>
2021-04-06 19:39:26 +0900

    no message

--- 9/README.md
+++ 9/README.md
@@ -60,8 +60,52 @@ npx 명령어를 사용하는 이유는 전역 설치를 피하기 위해서다.
 시퀄라이즈의 비밀번호를 숨기는 방법은 15.1.2 절에서 알아보자.
 
 기본적인 라우터와 템플릿 엔진도 만들어보자.
-routes 폴더 안에는 page.js를 , views 폴더 안에는 layout.html, main.html, join.html, error.html을 생성한다.
+routes 폴더 안에는 page.js를 , views 폴더 안에는 layout.html, main.html, join.html, error.html, profile.html을 생성한다.
 약간의 디자인을 위해 main.css를 public 폴더 안에 생성한다.
+
+    page.js
+
+GET /profile, GET /join, GET /까지 총 세 개의 페이지로 구성되어 있다.
+router.use 로 라우터용 미들웨어를 만들어 템플릿 엔진에서 사용할 user, followingCount, followerCount, followerIdList 변수를 res.locals로 설정했다.
+지금은 각각 null,0,0,[]이지만 나중에 값을 넣을 것이다.
+res.locals로 설정하는 이유는 user,followingCount,followerCount,followerIdList 변수를 모든 템플릿 엔진에서 공통으로 사용하기 때문이다.
+render 메서드 안의 twits도 지금은 빈 배열이지만 나중에 값을 넣는다.
+그 다음은 클라이언트 코드이다.
+
+    layout.html
+
+layout.html 에서는 if 문을 중점적으로 보면 된다.
+렌더링할 때 user가 존재하면 사용자 정보와 팔로잉, 팔로워 수를 보여주고, 존재하지 않으면 로그인 메뉴를 보여준다.
+
+    main.html
+
+main.html 에서는 user 변수가 존재할 때 게시글 업로드 폼을 보여준다.
+for 문도 추가되었다.
+렌더링 시 twits 배열 안의 요소들을 읽어서 게시글로 만든다.
+지금은 빈 배열이지만 나중에 twits에 게시글 데이터를 넣으면 된다.
+if not followerIdList.includes(twit.User.id) and twit.User.id !== user.id는 나의 팔로워 아이디 목록에 게시글 작성자의 아이디가 없으면 팔로우 버튼을 보여주기 위한 구문이다.
+또한 게시글 작성자가 나인 경우 나를 팔로우 할 수는 없게 했다.
+if not 과 nad 를 써서 여러 가지 조건들을 조합했다. 
+넌적스 문법이다.
+
+    profile.html
+
+profile.html 은 사용자의 팔로워와 사용자가 팔로잉 중인 목록을 보여준다.
+
+    join.html
+
+join.html 은 회원가입하는 폼을 보여준다.
+
+    error.html
+
+error.html 은 서버에 에러가 발생했을 때 에러 내역을 보여준다.
+에러는 콘솔로 봐도 되지만 브라우저 화면으로 보면 좀 더 편리하다.
+단, 배포시에는 에러 내용을 보여주지 않는 게 보안상 좋다.
+마지막으로 디자인을 위한 CSS 파일이다.
+
+    main.css
+
+이제 npm start 로 서버를 실행하고 8001번 포트에 접속해보자
 
 
 
--- 9/nodebird/app.js
+++ 9/nodebird/app.js
@@ -1,36 +1,38 @@
 const express = require('express');
-const path = require('path');
+const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
 const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
 
-const { sequelize } = require('./models');
-const indexRouter = require('./routes');
-const usersRouter = require('./routes/users');
-const commentsRouter = require('./routes/comments');
+dotenv.config();
+const pageRouter = require('./routes/page');
 
 const app = express();
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
   express: app,
   watch: true,
 });
-sequelize.sync({ force: false })
-  .then(() => {
-    console.log('데이터베이스 연결 성공');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/comments', commentsRouter);
+app.use('/', pageRouter);
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -46,5 +48,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기 중');
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
